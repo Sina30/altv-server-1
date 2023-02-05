f8d478e4f29c2dd020d732df6ebfbcb3f7600c41
william-valenduc <william.valenduc@orange.fr>
2023-02-05 20:05:32 +0100

    time command notif output

--- resources/time-weather/server/main.js
+++ resources/time-weather/server/main.js
@@ -1,153 +1,138 @@
-import * as alt from "alt-server"
-import * as db from "database"
-import * as chat from "chat"
-import Clock from "./Clock"
+import * as alt from "alt-server";
+import * as db from "database";
+import * as chat from "chat";
+import Clock from "./Clock";
 
+let clock;
 
-let clock
-
-alt.on("resourceStart", initClock)
-alt.on("ConnectionComplete", initTimeWeather)
-alt.on("save", saveServerData)
+alt.on("resourceStart", initClock);
+alt.on("ConnectionComplete", initTimeWeather);
+alt.on("save", saveServerData);
 
 alt.on("resourceStop", () => {
-    alt.setMeta("time", clock.getTime())
+    alt.setMeta("time", clock.getTime());
     //alt.setMeta("weather", )
-})
+});
 
 alt.on("playerConnect", (player) => {
-    alt.emitClient(player, "time:setTime", clock.getTime())
-    alt.emitClient(player, "time:setSpeed", clock.speed)
-    if (clock.started) 
-        alt.emitClient(player, "time:start")
-    else
-        alt.emitClient(player, "time:stop")
-})
+    alt.emitClient(player, "time:setTime", clock.getTime());
+    alt.emitClient(player, "time:setSpeed", clock.speed);
+    if (clock.started) alt.emitClient(player, "time:start");
+    else alt.emitClient(player, "time:stop");
+});
 
-function saveLog (msg) {
-    alt.log("~g~" + msg)
+function saveLog(msg) {
+    alt.log("~g~" + msg);
 }
 
-
-function initClock () {
-    clock = new Clock()
-    clock.start()
-    if (db.isReady()) 
-        initTimeWeather()
+function initClock() {
+    clock = new Clock();
+    clock.start();
+    if (db.isReady()) initTimeWeather();
 }
 
-async function initTimeWeather () {
-    const time = alt.getMeta("time")
-    if (time)
-        clock.setTime(time.h, time.m)
-    else
-        setDatabaseData()
+async function initTimeWeather() {
+    const time = alt.getMeta("time");
+    if (time) clock.setTime(time.h, time.m);
+    else setDatabaseData();
 }
 
-function setDatabaseData () {
+function setDatabaseData() {
     db.fetchData("id", 1, "Server", (server) => {
         if (!server) {
-            registerServerData()
-            return
+            registerServerData();
+            return;
         }
-        const {time, weather} = server
-        const {h, m} = JSON.parse(time)
-        clock.setTime(h, m)
+        const { time, weather } = server;
+        const { h, m } = JSON.parse(time);
+        clock.setTime(h, m);
         //weather
-    })
+    });
 }
 
-function registerServerData () {
-    const time = {h: 12, m: 0}
-    const weather = 1
-    db.upsertData({ id: 1, time: JSON.stringify(time), weather: weather }, 'Server', callback => {})
-    clock.setTime(time.h, time.m)
+function registerServerData() {
+    const time = { h: 12, m: 0 };
+    const weather = 1;
+    db.upsertData({ id: 1, time: JSON.stringify(time), weather: weather }, "Server", (callback) => {});
+    clock.setTime(time.h, time.m);
 }
 
-function saveServerData () {
-    db.updatePartialData(1, {time: JSON.stringify(clock.getTime()), weather: 1}, "Server", callback => {
-        saveLog("Time and Weather saved")
-    })
+function saveServerData() {
+    db.updatePartialData(1, { time: JSON.stringify(clock.getTime()), weather: 1 }, "Server", (callback) => {
+        saveLog("Time and Weather saved");
+    });
 }
 
-function timeCommand (time) {
-    let hour
+function timeCommand(time) {
+    let hour;
     switch (!isNaN(time) || time) {
-
         case time >= 0 && time < 24:
-            hour = time
+            hour = time;
             break;
 
         case "day":
-            hour = 12
+            hour = 12;
             break;
 
         case "night":
-            hour = 0
+            hour = 0;
             break;
-    
+
         default:
-            chat.send(player, '{ff8f00}Entier compris entre 0 et 23 attendu')
-            return
+            alt.emitClient(player, "notification", "command", "Entier compris entre 0 et 23 attendu");
+            return;
     }
-    clock.setTime(hour, 0)
-    chat.broadcast(`{00FFFF}Changement d'heure, il est actuellement ${hour}h`)
+    clock.setTime(hour, 0);
+    alt.emitAllClients("notification", "success", `Changement d'heure, il est actuellement ${hour}h`);
 }
 
 chat.registerCmd("time", (player, [parameter, value]) => {
     if (value != undefined && isNaN(value)) {
-        chat.send(player, "{555555}/clock [parameter] [value]")
-        return
+        alt.emitClient(player, "notification", "command", "/time [parameter] [value]");
+        return;
     }
-    value = parseInt(value)
+    value = parseInt(value);
 
     switch (parameter) {
-
         case "set":
-            timeCommand(value)
+            timeCommand(value);
             break;
 
         case "start":
         case "resume":
             if (clock.started) {
-                chat.send(player, "{ff8f00}Clock already running")
-                return
+                alt.emitClient(player, "notification", "command", "L'horloge est déjà en marche");
+                return;
             }
-            clock.start()
-            chat.broadcast('{00ffff}Clock {ffff00}started')
+            clock.start();
+            alt.emitAllClients("notification", "success", "L'horloge a démarré");
             break;
-        
+
         case "stop":
         case "pause":
             if (!clock.started) {
-                chat.send(player, "{ff8f00}Clock already stopped")
-                return
+                alt.emitClient(player, "notification", "command", "L'horloge est déjà en pause");
+                return;
             }
-            clock.stop()
-            chat.broadcast('{00ffff}Clock {ffff00}stopped')
+            clock.stop();
+            alt.emitAllClients("notification", "success", "L'horloge s'est arrêtée");
             break;
-        
+
         case "speed":
             if (value < 1 || value > 100) {
-                chat.send(player, "{ff8f00}Speed must be a number in between 1 and 100")
-                return
+                alt.emitClient(player, "notification", "command", "La vitesse doit être un nombre entre 1 et 100");
+                return;
             }
-            clock.setSpeed(value)
-            chat.broadcast(`{00ffff}Clock speed at {ffff00}x${value}`)
+            clock.setSpeed(value);
+            alt.emitAllClients("notification", "success", `Vitesse de l'horloge x${value}`);
             break;
-        
+
         case "help":
-        case undefined:
-            chat.send(player, "{555555} /clock [param]")
-            chat.send(player, "{555555} /clock start")
-            chat.send(player, "{555555} /clock stop")
-            chat.send(player, "{555555} /clock speed [1-100]")
+            alt.emitClient(player, "notification", "success", "/time [param]\n/time start\n/time stop\n/time speed [1-100]\n");
             break;
 
         default:
-            chat.send(player, "{ff8f00} Wrong usage")
-            return
-
+            alt.emitClient(player, "notification", "command", "Mauvais usage\n/time help pour plus d'info");
+            return;
     }
-
-})
+});
