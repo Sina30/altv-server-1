78507ba8a52e80de12b28b903b904dd91c503b0a
william-valenduc <william.valenduc@orange.fr>
2023-02-10 16:59:18 +0100

    models add
    ada wong
    lara croft
    chucky
    shrek

--- resources/playerManager/playerWebview/apps/model/data/mods.js
+++ resources/playerManager/playerWebview/apps/model/data/mods.js
@@ -5,4 +5,46 @@ const mods = [
         Pedtype: "mods",
         French: "John Wick",
     },
+    {
+        Name: "ada-wong",
+        Hash: 2732622891,
+        Pedtype: "mods",
+        French: "Ada Wong ",
+    },
+    {
+        Name: "ada-wong-re6",
+        Hash: 2693396285,
+        Pedtype: "mods",
+        French: "Ada Wong RE6",
+    },
+    {
+        Name: "lara-a",
+        Hash: 2349832680,
+        Pedtype: "mods",
+        French: "Lara A",
+    },
+    {
+        Name: "lara-b",
+        Hash: 2114780643,
+        Pedtype: "mods",
+        French: "Lara B",
+    },
+    {
+        Name: "lara-c",
+        Hash: 1858756446,
+        Pedtype: "mods",
+        French: "Lara C",
+    },
+    {
+        Name: "shrek",
+        Hash: 2989278028,
+        Pedtype: "mods",
+        French: "Shrek",
+    },
+    {
+        Name: "chucky",
+        Hash: 2404026180,
+        Pedtype: "mods",
+        French: "Chucky",
+    },
 ];
