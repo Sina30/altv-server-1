b8c840f9b0ef862619b89777336f791968747891
william-valenduc <william.valenduc@orange.fr>
2023-05-04 03:55:43 +0200

    getpos command

--- resources/main/server/commands/info.js
+++ resources/main/server/commands/info.js
@@ -5,12 +5,28 @@ alt.Utils.registerCommand({
     description: "Affiche les informations du joueur",
     args: [],
     execute: (player, []) => {
-        alt.log(`Nom: ${player.name} ID: ${player.getSyncedMeta("id")} OP: ${player.getMeta("op")}`);
+        alt.log(`Nom: ${player.name} ID: ${player.getSyncedMeta("id")} OP: ${player.getSyncedMeta("op")}`);
         player.notify({
             imageName: "CHAR_MP_FM_CONTACT",
             headerMsg: `Infos`,
             detailsMsg: player.name,
-            message: `ID: ${player.getSyncedMeta("id")}\nOP: ${player.getMeta("op")}`,
+            message: `ID: ${player.getSyncedMeta("id")}\nOP: ${player.getSyncedMeta("op")}`,
+        });
+    },
+});
+
+alt.Utils.registerCommand({
+    name: "getpos",
+    description: "Affiche la position du joueur",
+    args: [],
+    execute: (player, []) => {
+        alt.log(`Position: ${player.pos}`);
+        const pos = player.pos.toFixed(2);
+        player.notify({
+            imageName: "CHAR_MP_FM_CONTACT",
+            headerMsg: `Position`,
+            detailsMsg: player.name,
+            message: `Position:\nx: ~y~${pos.x}\n~w~y: ~y~${pos.y}\n~w~z: ~y~${pos.z}`,
         });
     },
 });
