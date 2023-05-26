366a55637fbc90df5c727f886bfe3c3aeb4ee24a
darco2903 <william.valenduc@orange.fr>
2023-05-26 20:49:39 +0200

    console commands
    list of commands:
    stop-server : stop the server
    r : restart the main resource
    help: display all commands

---
+++ resources/main/server/consoleCommand.js
@@ -0,0 +1,36 @@
+import * as alt from "alt-server";
+
+alt.on("serverStarted", () => {
+    alt.log("Type ~y~/help~w~ to show the list of commands.");
+});
+
+alt.on("consoleCommand", (name, args) => {
+    switch (name) {
+        case "stop-server":
+            alt.log("~y~Stopping server...");
+            alt.stopServer();
+            break;
+
+        case "r":
+            alt.log("~y~Restarting main resource...");
+            alt.restartResource("main");
+            break;
+
+        case "help":
+        case "h":
+        case "?":
+            alt.log(
+                "\n",
+                "~g~Available commands:\n",
+                "~y~- stop-server:     Stop the server\n",
+                "- r:               Reload the main resource\n",
+                "- clearveh:        Clear all non player vehicles\n",
+                "- help:            Show this help message"
+            );
+            break;
+
+        default:
+            alt.log(`Command "${name}" does not exist.`);
+            break;
+    }
+});
