d66a6fe4c02b667f5dc8b61e2535d9b14d8e82e6
darco2903 <william.valenduc@orange.fr>
2023-05-28 19:50:43 +0200

    fix player connect

--- resources/main/server/index.js
+++ resources/main/server/index.js
@@ -2,4 +2,4 @@ import "./consoleCommand.js";
 import "./core/chat/index.js";
 import "./core/player/notification.js";
 import "./core/player/permission.js";
-import "./gameplay/player/connection.js";
+import "./gameplay/player/connect.js";
