4cb48b67119f48b2eb91803db9da242c15a06542
darco2903 <william.valenduc@orange.fr>
2023-05-29 06:21:59 +0200

    player connect update

--- resources/main/server/gameplay/player/connect.js
+++ resources/main/server/gameplay/player/connect.js
@@ -2,8 +2,7 @@ import * as alt from "alt-server";
 
 alt.on("playerConnect", (player) => {
     alt.log(`~b~${player.name} ~g~(${player.socialID}) ~y~connecting...`);
-    player.spawn(0, 5, 71);
-    player.model = "mp_m_freemode_01";
+    player.spawn("mp_m_freemode_01", new alt.Vector3([180, -1030, 28]));
     player.setDateTime(0, 0, 0, 10, 0, 0);
     player.setSyncedMeta("op", 4);
 });
