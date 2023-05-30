fc7ec47ad73aea1ad4b32dae58a6cf99125d7d67
darco2903 <william.valenduc@orange.fr>
2023-05-30 17:26:51 +0200

    vehspawn command update
    remove values
    remake execution to spawn the vehicle the player's right
    the function use the new spawn command

--- resources/main/server/gameplay/commands/vehspawn.js
+++ resources/main/server/gameplay/commands/vehspawn.js
@@ -1,6 +1,4 @@
 import * as alt from "alt-server";
-// import vehicles from "../../../../data/vehicles.json" assert { type: "json" };
-// import vehiclesMod from "../../../../data/vehiclesMod.json" assert { type: "json" };
 
 alt.Utils.Chat.registerCmd({
     name: "vehspawn",
@@ -10,17 +8,15 @@ alt.Utils.Chat.registerCmd({
             name: "model",
             description: "Vehicle model",
             type: "string",
-            // values: vehicles
-            //     .concat(vehiclesMod)
-            //     .map((v) => v.Name)
-            //     .sort((a, b) => a.localeCompare(b)),
             required: true,
         },
     ],
     permissionLevel: 0,
     execute(player, [model]) {
-        const pos = player.pos.add(0, -2, 0);
-        const vehicle = alt.Vehicle.spawn(model, pos);
-        player.setIntoVehicle(vehicle, 1);
+        const rot = player.rot;
+        const heading = rot.z;
+        const forward = new alt.Vector3(Math.cos(heading), Math.sin(heading), 0);
+        const pos = player.pos.add(forward.mul(3));
+        const vehicle = alt.Vehicle.spawn(model, pos, rot);
     },
 });
